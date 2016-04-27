58862c6621812afd7067bbf48db15a1f1dd3a5df
Gabriel <gabriel.simoes@gmail.com>
2016-04-27 09:54:01 -0300

    Disable image crop output to file.

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -319,13 +319,13 @@ void DataTransformer<Dtype>::Transform(const cv::Mat& cv_img,
   }
 
   TEST IMAGE CROP OUTPUT
-  if (phase_ == TRAIN){
-    std::string img_path = ("/home/gabriel/tmp/tmp_layer_imgs/img");
-    img_path = img_path + boost::lexical_cast<std::string>(rand()*10000);
-    std::string ext = (".jpg");
-    img_path = img_path + ext;
-    imwrite(img_path, cv_cropped_img);
-  }
+  // if (phase_ == TRAIN){
+  //   std::string img_path = ("/home/gabriel/tmp/tmp_layer_imgs/img");
+  //   img_path = img_path + boost::lexical_cast<std::string>(rand()*10000);
+  //   std::string ext = (".jpg");
+  //   img_path = img_path + ext;
+  //   imwrite(img_path, cv_cropped_img);
+  // }
 
   CHECK(cv_cropped_img.data);
 
